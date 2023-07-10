79aee4b767db7167af9a6ef5c1584e289a85f833
Nathan Feiler <79991107+Pyro569@users.noreply.github.com>
2023-07-09 22:44:32 -0400

    Made the printing function back end code so much better

--- Program.cs
+++ Program.cs
@@ -411,129 +411,22 @@ namespace TrashLang
                     }
                 }
 
-                char CharToPrint = ' ';
-
-                switch (LoopOps) //to make this extra annoying perhaps change around the characters to make say 2 plus an h and 3 plus a z?
+                string CharString = " abcdefghijklmnopqrstuvwxyz+-@!#$& ,. ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                if(LoopOps != 37)
                 {
-                    case 1:
-                        CharToPrint = 'a';
-                        break;
-                    case 2:
-                        CharToPrint = 'b';
-                        break;
-                    case 3:
-                        CharToPrint = 'c';
-                        break;
-                    case 4:
-                        CharToPrint = 'd';
-                        break;
-                    case 5:
-                        CharToPrint = 'e';
-                        break;
-                    case 6:
-                        CharToPrint = 'f';
-                        break;
-                    case 7:
-                        CharToPrint = 'g';
-                        break;
-                    case 8:
-                        CharToPrint = 'h';
-                        break;
-                    case 9:
-                        CharToPrint = 'i';
-                        break;
-                    case 10:
-                        CharToPrint = 'j';
-                        break;
-                    case 11:
-                        CharToPrint = 'k';
-                        break;
-                    case 12:
-                        CharToPrint = 'l';
-                        break;
-                    case 13:
-                        CharToPrint = 'm';
-                        break;
-                    case 14:
-                        CharToPrint = 'n';
-                        break;
-                    case 15:
-                        CharToPrint = 'o';
-                        break;
-                    case 16:
-                        CharToPrint = 'p';
-                        break;
-                    case 17:
-                        CharToPrint = 'q';
-                        break;
-                    case 18:
-                        CharToPrint = 'r';
-                        break;
-                    case 19:
-                        CharToPrint = 's';
-                        break;
-                    case 20:
-                        CharToPrint = 't';
-                        break;
-                    case 21:
-                        CharToPrint = 'u';
-                        break;
-                    case 22:
-                        CharToPrint = 'v';
-                        break;
-                    case 23:
-                        CharToPrint = 'w';
-                        break;
-                    case 24:
-                        CharToPrint = 'x';
-                        break;
-                    case 25:
-                        CharToPrint = 'y';
-                        break;
-                    case 26:
-                        CharToPrint = 'z';
-                        break;
-                    case 27:
-                        CharToPrint = '+';
-                        break;
-                    case 28:
-                        CharToPrint = '-';
-                        break;
-                    case 29:
-                        CharToPrint = '@';
-                        break;
-                    case 30:
-                        CharToPrint = '!';
-                        break;
-                    case 31:
-                        CharToPrint = '#';
-                        break;
-                    case 32:
-                        CharToPrint = '$';
-                        break;
-                    case 33:
-                        CharToPrint = '&';
-                        break;
-                    case 34:
-                        CharToPrint = ' ';
-                        break;
-                    case 35:
-                        CharToPrint = ',';
-                        break;
-                    case 36:
-                        CharToPrint = '.';
-                        break;
-                    case 37:
-                        CharToPrint = '\n';
-                        break;
+                    System.Console.Write(CharString[LoopOps]);
                 }
-                System.Console.Write(Convert.ToString(CharToPrint));
+                else if(LoopOps == 37)
+                {
+                    System.Console.Write("\n");
+                }
             }
             else
             {
                 Console.WriteLine("Error in code");
             }
         }
+
         static string PrintCommas(int amount)
         {
             string ReturnValue = "";
